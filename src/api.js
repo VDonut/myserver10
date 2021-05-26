cc65df132396076cfbda68fd6eeda640af10f922
john lundgren <john.lundgren.lexicon@gmail.com>
2021-05-26 07:01:58 +0200

    Added CORS

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const Post = require('./postSchema');
 
 require('dotenv').config();
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 const router = express.Router();
 
